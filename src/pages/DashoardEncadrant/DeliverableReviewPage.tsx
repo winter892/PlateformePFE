cf8db8ac6427df908028214f34c9b1cba0bf5336
winter892 <souadwinter453@gmail.com>
2025-03-13 14:22:01 +0100

    changes

--- src/pages/DashoardEncadrant/DeliverableReviewPage.tsx
+++ src/pages/DashoardEncadrant/DeliverableReviewPage.tsx
@@ -111,7 +111,7 @@ const DeliverableReviewPage = () => {
   return (
     
     <DashboardLayout>
-      <button onClick={() =>} className="mb-8 flex items-center p-2 rounded-md transition  hover:bg-violet-100">
+      <button className="mb-8 flex items-center p-2 rounded-md transition  hover:bg-violet-100">
                         <ArrowLeft className="w-5 h-5 mr-2" /> Retour
                       </button>
       <div className="flex h-[calc(100vh-7rem)] overflow-hidden">
